e45a40885c74b5868875379b81210e83e468b126
Cracklitosss <marroquinca17@gmail.com>
2024-04-18 00:57:34 -0600

    Emitir en tiempo real

--- src/infrastructure/services/WebSocketService.ts
+++ src/infrastructure/services/WebSocketService.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 import { ISensorData } from '../../domain/SensorData'; // Asegúrate de que la ruta es correcta
+import dotenv from 'dotenv';
 
+dotenv.config();
 export class WebSocketService {
   private socket: any;
 
@@ -9,19 +11,24 @@ export class WebSocketService {
   }
 
   private connect() {
-    const websocketUrl = "https://websocketserver-l5ok.onrender.com";
-    this.socket = io(websocketUrl);
+    // Asegurarse de que la variable de entorno existe y es un string
+    const websocketUrl = process.env.WEBSOCKET_URL;
+    if (!websocketUrl) {
+      throw new Error('WEBSOCKET_URL is not defined in the .env file');
+    }
+
+    this.socket = io(websocketUrl as string);
     this.socket.on('connect', () => {
       console.log('Connected to WebSocket server as client');
     });
 
     this.socket.on('connect_error', (error: any) => {
-        console.log('Connection Error:', error);
+      console.log('Connection Error:', error);
     });
   }
 
   public sendSensorData(sensorData: ISensorData) {
-   // console.log('Sending sensor data via WebSocket:', sensorData);
+    // console.log('Sending sensor data via WebSocket:', sensorData);
     this.socket.emit('sensorData', sensorData);
   }
 }
